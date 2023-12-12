0f8d9a54590cde16c556260671d1a89a85ac6efb
Absolomon <andersfs199@gmail.com>
2023-12-12 10:16:47 +0100

    Cleanup

--- camera.py
+++ camera.py
@@ -1,7 +1,3 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This scrtipt script..
-
 import cv2 as cv
 from imutils.video.pivideostream import PiVideoStream
 import time
@@ -11,8 +7,8 @@ import numpy as np
 class VideoCamera(object):
     def __init__(self, file_type  = ".jpg", photo_string= "stream_photo"):
         self.vs = PiVideoStream().start()
-        self.file_type = file_type # image type i.e. .jpg
-        self.photo_string = photo_string # Name to save the photo
+        self.file_type = file_type
+        self.photo_string = photo_string
         time.sleep(0.5)
 
     def __del__(self):
